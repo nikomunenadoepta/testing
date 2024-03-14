7dac54475491063f496a2a1ac5ed0c39cfc85a5a
M T <mt@Acer-Sanya.local>
2024-03-14 13:13:25 +0300

    first

---
+++ config/postcss.config.js
@@ -0,0 +1,49 @@
+const autoprefixer = require('autoprefixer');
+const purgecss = require('@fullhuman/postcss-purgecss');
+const whitelister = require('purgecss-whitelister');
+
+module.exports = {
+  plugins: [
+    autoprefixer(),
+    purgecss({
+      content: [
+        './layouts/**/*.html',
+        './content/**/*.md',
+      ],
+      safelist: [
+        'lazyloaded',
+        'table',
+        'thead',
+        'tbody',
+        'tr',
+        'th',
+        'td',
+        'h5',
+        'alert-link',
+        'container-xxl',
+        'container-fluid',
+        'offcanvas-backdrop',
+        'img-fluid',
+        'lazyload',
+        'blur-up',
+        'figcaption',
+        '_ratio',
+        'ratio-1x1',
+        'ratio-4x3',
+        'ratio-16x9',
+        'dark-inverted',
+        ...whitelister([
+          './assets/scss/components/_alerts.scss',
+          './assets/scss/components/_buttons.scss',
+          './assets/scss/components/_code.scss',
+          './assets/scss/components/_diagrams.scss',
+          './assets/scss/components/_syntax.scss',
+          './assets/scss/components/_search.scss',
+          './assets/scss/common/_dark.scss',
+          './node_modules/bootstrap/scss/_dropdown.scss',
+          './node_modules/katex/dist/katex.css',
+        ]),
+      ],
+    }),
+  ],
+}
---
+++ content/articles/_index.md
@@ -0,0 +1,9 @@
+---
+title: Все статьи
+description: null
+lead: null
+date: 2020-11-06T18:04:19+01:00
+lastmod: 2024-01-05T19:19:06.433Z
+draft: false
+images: []
+---
